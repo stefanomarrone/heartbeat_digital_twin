64cce239d90920a792f3f4831a844f0919c5ea0f
Stefano Marrone <stefano.marrone@unicampania.it>
2024-04-28 10:14:45 +0200

    The first play is done: moods are recognized by means of a Decision Tree. Simple Bayesian Networks were not able to deal with continuous to discrete variables. It represents a future work.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from model import Heart
 from inference import InferencedHeart
+from moods import Mood
 import numpy as np
 
 
@@ -10,9 +11,9 @@ def inferencing(initialconditions, time, realdata):
     return results
 
 
-def modelling(initialconditions, time):
+def modelling(initialconditions, time, condition):
     x0, b0 = initialconditions
-    heart = Heart(x0, b0)
+    heart = Heart(x0, b0, condition)
     data = heart.beat(time)
     return data
 
@@ -20,7 +21,10 @@ def modelling(initialconditions, time):
 if __name__ == '__main__':
     initial = (0,0)
     t = np.linspace(0, 3600, 3600*10)
-    results = modelling(initial, t)
+    results = modelling(initial, t, 'coffee')
     inferenced = inferencing(initial, t, results)
+    mood = Mood()
+    results = mood.getmostlikelymood(inferenced)
+    print(results)
 
 
--- src/moods.py
+++ src/moods.py
@@ -1,6 +1,10 @@
-from pgmpy.models import BayesianNetwork
-from pgmpy.estimators import MaximumLikelihoodEstimator
-from pgmpy.inference import VariableElimination
+from sklearn import tree
+import pandas as pd
+import matplotlib.pyplot as plt
+from six import StringIO
+import pydotplus
+from sklearn.tree import export_graphviz
+
 
 class Mood:
     def __init__(self):
@@ -10,28 +14,47 @@ class Mood:
             'coffee': {'eps': 0.001, 'a': .81, 'xa': .45}
         }
         self.nominal = 'normal'
-        couples = self.getcouples()
-        self.classifier = BayesianNetwork(couples)
-        data = self.getdata()
-        self.classifier.fit(data, estimator=MaximumLikelihoodEstimator)
-
-    def getcouples(self):
-        keys = self.database[self.nominal].keys()
-        retval = list(map(lambda x: (x, 'mood'),keys))
-        return retval
-
+        self.classifier = tree.DecisionTreeClassifier()
+        features, target = self.getdata()
+        self.classifier = self.classifier.fit(features, target)
+        self.draw()
 
     def getdata(self):
-        keys = list(self.database[self.nominal].keys())
-        keys.append('mood')
-
-        return None
-
+        parameternames = self.database[self.nominal].keys()
+        column = list(parameternames)
+        column.append('mood')
+        df = pd.DataFrame(columns=column)
+        moods = self.database.keys()
+        i = 0
+        for mood in moods:
+            df.at[i, 'mood'] = mood
+            for parametername in parameternames:
+                df.at[i, parametername] = self.database[mood][parametername]
+            i += 1
+        X = df[parameternames]
+        y = df['mood']
+        return X, y
 
     def getparameters(self, label):
-        dictionary = self.database.get(label,self.nominal)
+        dictionary = self.database.get(label, self.nominal)
         return dictionary
 
-    def getmlmood(self, dictionary):
-        #todo you have to implement the method considering the construction of the Bayesian Network model
-        pass
+    def draw(self):
+        dot_data = StringIO()
+        feature_cols = list(self.database[self.nominal].keys())
+        target_names = list(self.database.keys())
+        export_graphviz(self.classifier, out_file=dot_data,
+                        filled=True, rounded=True,
+                        special_characters=True, feature_names=feature_cols,
+                        class_names=target_names)
+        graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+        graph.write_pdf('moodclassifier.pdf')
+
+
+    def getmostlikelymood(self, dictionary):
+        parameternames = list(self.database[self.nominal].keys())
+        df = pd.DataFrame(columns=parameternames)
+        for parametername in parameternames:
+            df.at[0, parametername] = dictionary[parametername]
+        y_pred = self.classifier.predict(df)
+        return y_pred
